6f0f98002dfaee2c2b645040e07d080edb95b2ed
jerrykid <ngdinhthi@gmail.com>
2017-10-25 13:02:21 +0700

    initial app

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAzureLab.Startup))]
+namespace WebAzureLab
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
